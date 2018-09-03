fafd62214a52da830d2090ef3d785b6f276b2956
PaulakaPaul <p.e.iusztin@gmail.com>
2018-09-03 15:41:41 +0300

    Changed user pool

---
+++ app/src/main/java/fusionkey/lowkey/main/profile/EditUserActivity.java
@@ -0,0 +1,15 @@
+package fusionkey.lowkey.main.profile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fusionkey.lowkey.R;
+
+public class EditUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user);
+    }
+}
