5625eb69dd3a3cb8bf44dfe6f6c9408f77d7da86
paulakapaul <p.e.iusztin@gmail.com>
2018-10-27 18:40:26 +0300

    Solve 0 points no display bug.

--- app/src/main/java/fusionkey/lowkey/newsfeed/profileFragment.java
+++ app/src/main/java/fusionkey/lowkey/newsfeed/profileFragment.java
@@ -103,7 +103,8 @@ public class profileFragment extends Fragment {
 
             Double experience = (double) pointsS;
 
-            points.setText(pointsS != 0L ? pointsS + "" : "");
+            String pointsDisplay = pointsS + "";
+            points.setText(pointsDisplay);
 
             paymentBar.setMax(2500);
             paymentBar.setProgress((int)experience.doubleValue());
