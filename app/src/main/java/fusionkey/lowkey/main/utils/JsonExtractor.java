f903cf00708d5cddd1222513a9402fbcc0e9eb6a
Iusztin Paul <p.e.iusztin@gmail.com>
2018-11-17 23:51:59 +0200

    Refactor JsonExtractor.

--- app/src/main/java/fusionkey/lowkey/main/utils/JsonExtractor.java
+++ app/src/main/java/fusionkey/lowkey/main/utils/JsonExtractor.java
@@ -13,7 +13,7 @@ public class JsonExtractor {
 
     public String extractString(String key) {
         try {
-            return (String) extract(JsonTypes.STRING, key);
+            return data.getString(key);
         } catch (JSONException e) {
             return "";
         }
@@ -21,7 +21,7 @@ public class JsonExtractor {
 
     public Boolean extractBoolean(String key) {
         try {
-            return (Boolean) extract(JsonTypes.BOOLEAN, key);
+            return data.getBoolean(key);
         } catch (JSONException e) {
             return false;
         }
@@ -29,7 +29,7 @@ public class JsonExtractor {
 
     public Integer extractInteger(String key) {
         try {
-            return (Integer) extract(JsonTypes.INTEGER, key);
+            return data.getInt(key);
         } catch (JSONException e) {
             return 0;
         }
@@ -37,44 +37,9 @@ public class JsonExtractor {
 
     public Long extractLong(String key) {
         try {
-            return (Long) extract(JsonTypes.LONG, key);
+            return data.getLong(key);
         } catch (JSONException e) {
             return 0L;
-        }
-    }
-
-    private Object extract(JsonTypes type, String key) throws JSONException{
-            if (type.equals(JsonTypes.STRING))
-                return data.getString(key);
-            else if (type.equals(JsonTypes.BOOLEAN))
-                return data.getBoolean(key);
-            else if(type.equals(JsonTypes.INTEGER))
-                return data.getInt(key);
-            else if(type.equals(JsonTypes.LONG))
-                return data.getLong(key);
-
-        throw new JSONException("Not supported data");
-    }
-
-
-
-    private enum JsonTypes {
-        STRING("string"),
-        BOOLEAN("bool"),
-        INTEGER("integer"),
-        LONG("long")
-        ;
-
-        private String attribute;
-
-        JsonTypes(String attribute) {
-            this.attribute = attribute;
-        }
-
-
-        @Override
-        public String toString() {
-            return super.toString();
         }
     }
 }
